cdb5afbbd605b1fdcc8f465ae3b041c50ee1cbe0
yargrinders <yargrindersp20@gmail.com>
2022-08-18 17:56:41 +0200

    Relvise

--- js/script.js
+++ js/script.js
@@ -1,10 +1,22 @@
-$(document).ready(function() {
-    $('.header__burger').click(function(event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
-    });
-    $('.header__menu').click(function(){
-        $('.header__burger, .header__menu').removeClass('active');
-        $('body').removeClass('lock');
-    });
-});
+// $(document).ready(function() {
+//     $('.header__burger').click(function(event) {
+//         $('.header__burger,.header__menu').toggleClass('active');
+//         $('body').toggleClass('lock');
+//     });
+//     $('.header__menu').click(function(){
+//         $('.header__burger, .header__menu').removeClass('active');
+//         $('body').removeClass('lock');
+//     });
+// });
+
+function ibg(){
+
+    let ibg=document.querySelectorAll(".ibg");
+    for (var i = 0; i < ibg.length; i++) {
+    if(ibg[i].querySelector('img')){
+    ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+    }
+    }
+    }
+    
+    ibg();
